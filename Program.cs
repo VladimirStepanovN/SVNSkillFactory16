8fad6ecde62e3cf29e1a2fd959c8253a89b52a19
VladimirStepanovN <Vladimir.Stepanov.Dev@yandex.ru>
2023-06-08 01:13:19 +0500

    Задание 6.2.2.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Pen p = new Pen();
+            Console.WriteLine($"цвет: {p.color}, стоимость: {p.cost}");
+            Pen p2 = new Pen("Зелёный", 150);
+            Console.WriteLine($"цвет: {p2.color}, стоимость: {p2.cost}");
+        }
+        class Pen
+        {
+            public string color;
+            public int cost;
+            public Pen()
+            {
+                color = "Черный";
+                cost = 100;
+            }
+            public Pen(string color, int cost)
+            {
+                this.color = color;
+                this.cost = cost;
+            }
         }
     }
 }
